368951d0f2567bf0801cf067a8d45ab9e4e3fecd
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-03 14:13:20 -0300

    fix(migration): update CreateCalls migration to set floor, direction as not null and status default to 'pending'

--- elevator_backend/db/migrate/20240803170705_create_calls.rb
+++ elevator_backend/db/migrate/20240803170705_create_calls.rb
@@ -1,10 +1,9 @@
-class CreateCalls < ActiveRecord::Migration[7.1]
+class CreateCalls < ActiveRecord::Migration[7.0]
   def change
     create_table :calls do |t|
-      t.integer :floor
-      t.string :direction
-      t.string :status
-
+      t.integer :floor, null: false
+      t.string :direction, null: false
+      t.string :status, default: 'pending'
       t.timestamps
     end
   end
