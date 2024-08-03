1bba75c2661cc93f10a08e6c22173d2b8e03aedd
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-03 15:43:49 -0300

    fix(routes): update routes for ElevatorController and add favicon route

--- elevator_backend/config/routes.rb
+++ elevator_backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :calls, only: [:index, :create, :update, :destroy]
   resources :logs, only: [:index, :create]
-  put 'elevator/arrive', to: 'elevator#arrive'
+  post 'arrive', to: 'elevator#arrive'
+  get '/favicon.ico', to: proc { [204, {}, []] }
 end
