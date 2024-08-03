63ea61ae5ae58c63a729e3e1da586b780b5a194f
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-03 15:25:24 -0300

    feat(controller): implement LogsController with index and create actions for ElevatorLog

---
+++ elevator_backend/app/controllers/logs_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class LogsController < ApplicationController
+  def index
+    @logs = ElevatorLog.all
+    render json: @logs
+  end
+
+  def create
+    @log = ElevatorLog.new(log_params)
+    if @log.save
+      render json: @log, status: :created
+    else
+      render json: @log.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def log_params
+    params.require(:elevator_log).permit(:from_floor, :to_floor, :direction, :timestamp)
+  end
+end
