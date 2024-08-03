691747a6b94ec2642fc80c01880701128cdd4b8c
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-03 14:17:47 -0300

    fix(migration): update CreateElevatorLogs migration to add constraints and columns

--- elevator_backend/db/migrate/20240803171540_create_elevator_logs.rb
+++ elevator_backend/db/migrate/20240803171540_create_elevator_logs.rb
@@ -1,9 +1,9 @@
 class CreateElevatorLogs < ActiveRecord::Migration[7.1]
   def change
     create_table :elevator_logs do |t|
-      t.integer :floor
-      t.string :direction
-      t.datetime :timestamp
+      t.integer :floor, null: false
+      t.string :direction, null: false
+      t.datetime :timestamp, null: false
       t.integer :from_floor
       t.integer :to_floor
 
